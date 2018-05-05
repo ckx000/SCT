e36e86455d657d41ade16b39f3de434e0a4087c1
ckx000 <ckx000@gmail.com>
2018-05-06 01:16:17 +0800

    Delete casino_slot.js

--- casino_slot.js
+++
@@ -1,9 +0,0 @@
-
-var dqdddddd = {
-	start:function(){
-		var a="\x63\x79\u53cd\u5916\u6302\u66f4\u65b0\u4e86\x2c\u6682\u65f6\u4e0b\u7ebf\x2c\u7b49\u5f85\u66f4\u65b0";
-		$('<div class="dsfa" style="position:absolute;z-index:250001;top:0;right:0"><button style="width:110px">CoreVer: '+a+'</button></div>').appendTo(document.body);
-		setTimeout(dqdddddd.del,5000)
-	},del:function(){$('.dsfa').remove()}
-}
-dqdddddd.start();
